7197ffad3609c691078f082fc2090f842bddd226
marcelo <marc29dias@yahoo.com.br>
2020-09-27 20:50:02 -0300

    Alteração para usar o delay no lugar do serviço externo

--- ExternalServices/AssincronoService.cs
+++ ExternalServices/AssincronoService.cs
@@ -19,50 +19,104 @@ namespace ExAsyncApi.ExternalService
 
         public async Task<IEnumerable<Album>> GetAlbumsAsync()
         {
-            var responseMessage = await _httpClient.GetAsync($"albums/?nocache={Guid.NewGuid()}");
-            var responseText = await responseMessage.Content.ReadAsStringAsync();
-            var albums = JsonConvert.DeserializeObject<IEnumerable<Album>>(responseText);
-            return albums;
+            // var responseMessage = await _httpClient.GetAsync($"albums/?nocache={Guid.NewGuid()}");
+            // var responseText = await responseMessage.Content.ReadAsStringAsync();
+            // var albums = JsonConvert.DeserializeObject<IEnumerable<Album>>(responseText);
+            // return albums;
+
+            await Task.Delay(1000);
+
+            return new List<Album>
+            {
+                new Album{
+
+                }
+            };
         }
 
         public async Task<IEnumerable<Comment>> GetCommentsAsync()
         {
-            var responseMessage = await _httpClient.GetAsync($"comments/?nocache={Guid.NewGuid()}");
-            var responseText = await responseMessage.Content.ReadAsStringAsync();
-            var comments = JsonConvert.DeserializeObject<IEnumerable<Comment>>(responseText);
-            return comments;
+            // var responseMessage = await _httpClient.GetAsync($"comments/?nocache={Guid.NewGuid()}");
+            // var responseText = await responseMessage.Content.ReadAsStringAsync();
+            // var comments = JsonConvert.DeserializeObject<IEnumerable<Comment>>(responseText);
+            // return comments;
+
+            await Task.Delay(1000);
+
+            return new List<Comment>
+            {
+                new Comment{
+
+                }
+            };
         }
 
         public async Task<IEnumerable<Photo>> GetPhotosAsync()
         {
-            var responseMessage = await _httpClient.GetAsync($"photos/?nocache={Guid.NewGuid()}");
-            var responseText = await responseMessage.Content.ReadAsStringAsync();
-            var photos = JsonConvert.DeserializeObject<IEnumerable<Photo>>(responseText);
-            return photos;
+            // var responseMessage = await _httpClient.GetAsync($"photos/?nocache={Guid.NewGuid()}");
+            // var responseText = await responseMessage.Content.ReadAsStringAsync();
+            // var photos = JsonConvert.DeserializeObject<IEnumerable<Photo>>(responseText);
+            // return photos;
+
+            await Task.Delay(1000);
+
+            return new List<Photo>
+            {
+                new Photo{
+
+                }
+            };
         }
 
         public async Task<IEnumerable<Post>> GetPostsAsync()
         {
-            var responseMessage = await _httpClient.GetAsync($"posts/?nocache={Guid.NewGuid()}");
-            var responseText = await responseMessage.Content.ReadAsStringAsync();
-            var posts = JsonConvert.DeserializeObject<IEnumerable<Post>>(responseText);
-            return posts;
+            // var responseMessage = await _httpClient.GetAsync($"posts/?nocache={Guid.NewGuid()}");
+            // var responseText = await responseMessage.Content.ReadAsStringAsync();
+            // var posts = JsonConvert.DeserializeObject<IEnumerable<Post>>(responseText);
+            // return posts;
+
+            await Task.Delay(1000);
+
+            return new List<Post>
+            {
+                new Post{
+
+                }
+            };
         }
 
         public async Task<IEnumerable<Todo>> GetTodosAsync()
         {
-            var responseMessage = await _httpClient.GetAsync($"todos/?nocache={Guid.NewGuid()}");
-            var responseText = await responseMessage.Content.ReadAsStringAsync();
-            var todos = JsonConvert.DeserializeObject<IEnumerable<Todo>>(responseText);
-            return todos;
+            // var responseMessage = await _httpClient.GetAsync($"todos/?nocache={Guid.NewGuid()}");
+            // var responseText = await responseMessage.Content.ReadAsStringAsync();
+            // var todos = JsonConvert.DeserializeObject<IEnumerable<Todo>>(responseText);
+            // return todos;
+
+            await Task.Delay(1000);
+
+            return new List<Todo>
+            {
+                new Todo{
+
+                }
+            };
         }
 
         public async Task<IEnumerable<User>> GetUsersAsync()
         {
-            var responseMessage = await _httpClient.GetAsync($"users/?nocache={Guid.NewGuid()}");
-            var responseText = await responseMessage.Content.ReadAsStringAsync();
-            var users = JsonConvert.DeserializeObject<IEnumerable<User>>(responseText);
-            return users;
+            // var responseMessage = await _httpClient.GetAsync($"users/?nocache={Guid.NewGuid()}");
+            // var responseText = await responseMessage.Content.ReadAsStringAsync();
+            // var users = JsonConvert.DeserializeObject<IEnumerable<User>>(responseText);
+            // return users;
+
+            await Task.Delay(1000);
+
+            return new List<User>
+            {
+                new User{
+
+                }
+            };
         }
     }
 }
--- ExternalServices/SincronoService.cs
+++ ExternalServices/SincronoService.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Threading;
 using ExAsyncApi.Models;
 using Newtonsoft.Json;
 
@@ -17,50 +18,104 @@ namespace ExAsyncApi.ExternalService
         }
         public IEnumerable<Album> GetAlbums()
         {
-            var responseMessage = _httpClient.GetAsync($"albums/?nocache={Guid.NewGuid()}").Result;
-            var responseText = responseMessage.Content.ReadAsStringAsync().Result;
-            var albums = JsonConvert.DeserializeObject<IEnumerable<Album>>(responseText);
-            return albums;
+            // var responseMessage = _httpClient.GetAsync($"albums/?nocache={Guid.NewGuid()}").Result;
+            // var responseText = responseMessage.Content.ReadAsStringAsync().Result;
+            // var albums = JsonConvert.DeserializeObject<IEnumerable<Album>>(responseText);
+            // return albums;
+
+            Thread.Sleep(1000);
+
+            return new List<Album>
+            {
+                new Album{
+
+                }
+            };
         }
 
         public IEnumerable<Comment> GetComments()
         {
-            var responseMessage = _httpClient.GetAsync($"comments/?nocache={Guid.NewGuid()}").Result;
-            var responseText = responseMessage.Content.ReadAsStringAsync().Result;
-            var comments = JsonConvert.DeserializeObject<IEnumerable<Comment>>(responseText);
-            return comments;
+            // var responseMessage = _httpClient.GetAsync($"comments/?nocache={Guid.NewGuid()}").Result;
+            // var responseText = responseMessage.Content.ReadAsStringAsync().Result;
+            // var comments = JsonConvert.DeserializeObject<IEnumerable<Comment>>(responseText);
+            // return comments;
+
+            Thread.Sleep(1000);
+
+            return new List<Comment>
+            {
+                new Comment{
+
+                }
+            };
         }
 
         public IEnumerable<Photo> GetPhotos()
         {
-            var responseMessage = _httpClient.GetAsync($"photos/?nocache={Guid.NewGuid()}").Result;
-            var responseText = responseMessage.Content.ReadAsStringAsync().Result;
-            var photos = JsonConvert.DeserializeObject<IEnumerable<Photo>>(responseText);
-            return photos;
+            // var responseMessage = _httpClient.GetAsync($"photos/?nocache={Guid.NewGuid()}").Result;
+            // var responseText = responseMessage.Content.ReadAsStringAsync().Result;
+            // var photos = JsonConvert.DeserializeObject<IEnumerable<Photo>>(responseText);
+            // return photos;
+
+            Thread.Sleep(1000);
+
+            return new List<Photo>
+            {
+                new Photo{
+
+                }
+            };
         }
 
         public IEnumerable<Post> GetPosts()
         {
-            var responseMessage = _httpClient.GetAsync($"posts/?nocache={Guid.NewGuid()}").Result;
-            var responseText = responseMessage.Content.ReadAsStringAsync().Result;
-            var posts = JsonConvert.DeserializeObject<IEnumerable<Post>>(responseText);
-            return posts;
+            // var responseMessage = _httpClient.GetAsync($"posts/?nocache={Guid.NewGuid()}").Result;
+            // var responseText = responseMessage.Content.ReadAsStringAsync().Result;
+            // var posts = JsonConvert.DeserializeObject<IEnumerable<Post>>(responseText);
+            // return posts;
+
+            Thread.Sleep(1000);
+
+            return new List<Post>
+            {
+                new Post{
+
+                }
+            };
         }
 
         public IEnumerable<Todo> GetTodos()
         {
-            var responseMessage = _httpClient.GetAsync($"todos/?nocache={Guid.NewGuid()}").Result;
-            var responseText = responseMessage.Content.ReadAsStringAsync().Result;
-            var todos = JsonConvert.DeserializeObject<IEnumerable<Todo>>(responseText);
-            return todos;
+            // var responseMessage = _httpClient.GetAsync($"todos/?nocache={Guid.NewGuid()}").Result;
+            // var responseText = responseMessage.Content.ReadAsStringAsync().Result;
+            // var todos = JsonConvert.DeserializeObject<IEnumerable<Todo>>(responseText);
+            // return todos;
+
+            Thread.Sleep(1000);
+
+            return new List<Todo>
+            {
+                new Todo{
+
+                }
+            };
         }
 
         public IEnumerable<User> GetUsers()
         {
-            var responseMessage = _httpClient.GetAsync($"users/?nocache={Guid.NewGuid()}").Result;
-            var responseText = responseMessage.Content.ReadAsStringAsync().Result;
-            var users = JsonConvert.DeserializeObject<IEnumerable<User>>(responseText);
-            return users;
+            // var responseMessage = _httpClient.GetAsync($"users/?nocache={Guid.NewGuid()}").Result;
+            // var responseText = responseMessage.Content.ReadAsStringAsync().Result;
+            // var users = JsonConvert.DeserializeObject<IEnumerable<User>>(responseText);
+            // return users;
+
+            Thread.Sleep(1000);
+
+            return new List<User>
+            {
+                new User{
+
+                }
+            };
         }
     }
 }
